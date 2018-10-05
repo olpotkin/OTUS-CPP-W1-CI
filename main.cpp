204c0a807ad07c15856e4a9e56729428a8dd50ba
Oleg Potkin <olpotkin@gmail.com>
2018-10-05 21:10:31 +0200

    fix: Change executable name and output

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("build {} \nHello, World!", version());
+    logger->info("build {}", version());
+    logger->info("Hello, World!");
 
     return 0;
 }
