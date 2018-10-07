0ae149e0b2e7658299d4435ecdc24fa4454e9566
Oleg Potkin <olpotkin@gmail.com>
2018-10-07 21:39:44 +0200

    fix: Replace logger with simple concole output

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("build {}", version());
-    logger->info("Hello, World!");
+    std::cout << "build " << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }
